ab3907896cef5d2398cbb0b80255688cee5a58a9
Igor Grillo Peternella <igor.feq@gmail.com>
2020-02-16 13:09:34 -0300

    Added templates folder. Changed views.py for web app.

--- django_async/apps/web/views.py
+++ django_async/apps/web/views.py
@@ -3,10 +3,11 @@ Views for the web app.
 """
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
     """
     Index page of the web app.
     """
-    return HttpResponse("hi", status=200)
+    return render(request, template_name="index.html", context={"user_name": "igp!"})

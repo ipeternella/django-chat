ea921ee8f487621f57a7f7741a5b43ac37ce2473
Igor Grillo Peternella <igor.feq@gmail.com>
2020-03-08 12:19:07 -0300

    Improved models

--- django_async/apps/web/models.py
+++ django_async/apps/web/models.py
@@ -1,3 +1,42 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class StandardModelMixin(models.Model):
+    """
+    Abstract class for models.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="Id")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
+
+    class Meta:
+        abstract = True
+
+
+class ChatUser(StandardModelMixin):
+    """
+    Models a simple chat message.
+    """
+
+    name = models.CharField(max_length=255)
+
+
+class ChatMessage(StandardModelMixin):
+    """
+    Models a simple chat message.
+    """
+
+    message = models.CharField(max_length=255)
+    user = models.OneToOneField(to=ChatUser, related_name="chat_user", on_delete=models.CASCADE)
+
+
+class ChatRoom(StandardModelMixin):
+    """
+    Models a simple chat room.
+    """
+
+    name = models.CharField(max_length=255)
+    messages = models.ForeignKey(to=ChatMessage, related_name="chat_room", on_delete=models.CASCADE)

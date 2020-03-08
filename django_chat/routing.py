2fb0e6acd95fe323f479187cc3a8e31716ca2bf4
Igor Grillo Peternella <igor.feq@gmail.com>
2020-03-08 19:42:39 -0300

    Added security stacks for websocket routing.

--- django_chat/routing.py
+++ django_chat/routing.py
@@ -5,10 +5,12 @@ with DIFFERENT protocols to be handled by different CONSUMERS (ASGI applications
 ProtocolTypeRouter itself is the main ASGI application that wraps many consumers. This
 application is served to Daphne ASGI app server.
 """
+from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
 from channels.routing import ChannelNameRouter
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django_chat.apps.chat.consumers import SMSChannelConsumer
 from django_chat.apps.chat.urls import websocket_urlpatterns
 
@@ -16,7 +18,7 @@ from django_chat.apps.chat.urls import websocket_urlpatterns
 application = ProtocolTypeRouter(
     {
         "http": AsgiHandler,
-        "websocket": URLRouter(websocket_urlpatterns),
+        "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
         "channel": ChannelNameRouter({"sms_channel": SMSChannelConsumer}),
     }
 )
